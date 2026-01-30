5631cc87f462bd740597da2870b08a5d60020bd3
oregonjoe <joe@chetcodigital.com>
2026-01-30 11:05:06 -0800

    add email confirm on new user

--- web/app.py
+++ web/app.py
@@ -530,6 +530,15 @@ def aws_login():
   
   session.clear()
 
+  cookie_name = 'session'
+  
+  response.delete_cookie(
+          cookie_name, 
+          path='/', # Match the original cookie path
+          httponly=True # It's good practice to keep the HttpOnly flag consistent
+      )
+
+
   #return oauth_aws.oidc.authorize_redirect('https://www.helmsmart-cloud.com/aws_alerts_get_user_data')
   #return oauth_aws.oidc.authorize_redirect('https://www.helmsmart-cloud.com/aws_alerts_get_user_data?login_hint=someone')
   #return oauth_aws.oidc.authorize_redirect('https://www.helmsmart-cloud.com/aws_alerts_get_user_data', authorize_params={'prompt': 'login'} )
